6b6beca81ab55aca484e5b6aee981f70f2b70f70
Tuana Celik <tuanacelik95@gmail.com>
2020-09-28 12:32:17 +0200

    reduced to slect cities to cope with limit

--- index.js
+++ index.js
@@ -6,66 +6,37 @@ var request = require('request');
 var air_quality = {};
 var tracked_cities = [];
 air_quality.tracked_cities = tracked_cities;
-
+var my_cities = {
+    "New York" : { "state" : "New York", "country" : "USA"},
+    "Paris" : { "state" : "Ile-de-France", "country" : "France"},
+    "London" : { "state" : "England", "country" : "UK"},
+    "Amsterdam" : { "state" : "North Holland", "country" : "Netherlands"},
+    "Leuven" : { "state" : "Flanders", "country" : "Belgium"},
+    "Istanbul" : { "state" : "Istanbul", "country" : "Turkey"},
+    "Hong Kong" : { "state" : "Hong Kong", "country" : "Hong Kong"}
+};
 // 1. List dataset
 app.get('/datasets', function(req, res) {   
-
-    if (req.headers['x-secret'] !== process.env.CUMULIO_SECRET)
-        return res.status(403).end('Given plugin secret does not match Cumul.io plugin secret.');
-    
-    request.get({
-        uri: `http://api.airvisual.com/v2/countries?key=${process.env.API_KEY}`,
-        gzip: true,
-        json: true
-        }, function(error, countries) {
-        if (error)
-            return res.status(500).end('Internal Server Error');
-        var datasets = countries.body.data.map(function(country) {
-            return {
-            id: country.country,
-            name: {en: `Air Quality for ${country.country}`},
-            description: {en: `Real-timeair quality data for ${country.country}`},
+    var datasets = [
+        {
+            id: "Air Visuals Data",
+            name: {en: `Air Quality for Select Cities`},
+            description: {en: `Real-time air quality data for select cities`},
             columns: [
-                {id: 'city', name: {en: 'City'}, type: 'hierarchy'},
-                {id: 'last_update', name: {en: 'Last update'}, type: 'datetime'},
-                {id: 'latitude', name: {en: 'Latitude'}, type: 'numeric'},
-                {id: 'longitude', name: {en: 'Longitude'}, type: 'numeric'},
-                {id: 'aqius', name: {en: 'Air Quality Index (US)'}, type: 'numeric'},
-                {id: 'aqcn', name: {en: 'Air Quality Index (China)'}, type: 'numeric'}
-            ]
-            }
-        });
-        return res.status(200).json(datasets);
-        });
+                    {id: 'city', name: {en: 'City'}, type: 'hierarchy'},
+                    {id: 'update', name: {en: 'Update time'}, type: 'datetime'},
+                    {id: 'latitude', name: {en: 'Latitude'}, type: 'numeric'},
+                    {id: 'longitude', name: {en: 'Longitude'}, type: 'numeric'},
+                    {id: 'aqius', name: {en: 'Air Quality Index (US)'}, type: 'numeric'},
+                    {id: 'aqcn', name: {en: 'Air Quality Index (China)'}, type: 'numeric'}
+                ]
+        }];
+    return res.status(200).json(datasets);
 });
 
-let getCities = (country, state, res) => {
-    return new Promise(
-        (resolve, reject) => {
-        request.get({
-            uri: `http://api.airvisual.com/v2/cities?state=${state}&country=${country}&key=${process.env.API_KEY}`,
-            gzip: true,
-            json: true
-        }, function(error, cities) {
-            if (error)
-                return reject(error);
-            
-            console.log(cities.body.data);
-            var available_cities = cities.body.data.map(function(city) {
-                return city.city;
-            });
-
-            var resolved_data = available_cities.map(function(city) {
-                return getAirQuality(country, state, city, res);
-            });
-            Promise.all(resolved_data).then(() => {
-                resolve();
-            });
-        });
-    });
-};
-
-let getAirQuality = (country, state, city, res) => {
+let getAirQuality = (city, res) => {
+    let state = my_cities[city]["state"];
+    let country = my_cities[city]["country"];
     return new Promise(
         (resolve, reject) => {
         request.get({
@@ -76,10 +47,12 @@ let getAirQuality = (country, state, city, res) => {
             if (error)
                 return reject(error);
             console.log(country + " " + state + " " + city);
-            console.log(airData.body.data.current);
-            var city_data = [city, airData.body.data.current.pollution.ts, airData.body.data.location.coordinates[0], airData.body.data.location.coordinates[1], airData.body.data.current.pollution.aqius, airData.body.data.current.pollution.aqicn];
-            tracked_cities.push(city_data);
-            resolve();
+            console.log(airData.body.data);
+            if(typeof airData.body.data.current !== 'undefined') {
+                var city_data = [city, airData.body.data.current.pollution.ts, airData.body.data.location.coordinates[1], airData.body.data.location.coordinates[0], airData.body.data.current.pollution.aqius, airData.body.data.current.pollution.aqicn];
+                tracked_cities.push(city_data);
+            }
+            setTimeout(function() {resolve();}, 1500);
         });
     });
 };
@@ -88,38 +61,15 @@ app.post('/query', function(req, res) {
     if (req.headers['x-secret'] !== process.env.CUMULIO_SECRET)
       return res.status(403).end('Given plugin secret does not match Cumul.io plugin secret.');
 
-    var country = req.body.id;
-    request.get({
-        uri: `http://api.airvisual.com/v2/states?country=${country}&key=${process.env.API_KEY}`,
-        gzip: true,
-        json: true
-    }, function(error, states) {
+    async function get_data(error) {
         if (error)
             return res.status(500).end('Internal Server Error');
-        
-        var states = states.body.data.map(function(state){
-            return state.state;
-        });
-
-        let getTrackedCityData = (states) => {
-            return new Promise(
-                (resolve, reject) => {
-                    if(error) reject(error);
-                    var resolved_cities = states.map(function(state) {
-                      //console.log(state.state);
-                       return getCities(country, state, res);
-                    });
-                    Promise.all(resolved_cities).then(() => {
-                        resolve(tracked_cities);
-                    });
-                }
-            )
+        for(var key of Object.keys(my_cities)){
+            await getAirQuality(key, res);
         }
-
-        getTrackedCityData(states).then(
-            tracked_cities => res.status(200).json(tracked_cities)
-        ).catch(
-            error => console.log(error)
-        );
-    });
+        console.log("TRACKED: "  + tracked_cities);
+        return res.status(200).json(tracked_cities);
+    };
+    get_data();
+           
   });
